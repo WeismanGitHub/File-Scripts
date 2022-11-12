de3ae16fbe5e53481ebd8a3e25938a04649ecaa8
Weisman <weisman123@outlook.com>
2022-11-12 04:09:40 -0800

    added to normalizing

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ watcher.on('add', function(path) {
     const name = parse(path).name
     const extension = parse(path).ext
     const dir = dirname(path)
-    const normalizedName = name.replaceAll('.', ' ').split('1080p')[0].split('WEBRip')[0].split('720p')[0].trim('')
+    const normalizedName = name.replaceAll('.', ' ').split('1080p')[0]
+    .split('WEBRip')[0].split('720p')[0].split('BDRip')[0].trim('')
     
     rename(path, resolve(dir, normalizedName + extension), (err) => {})
 })
