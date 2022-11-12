1d8e43172d64ae8fd274b9f2777edd8e9ed897cd
Weisman <weisman123@outlook.com>
2022-11-12 00:55:39 -0800

    finished

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { rename } = require("fs");
 require('dotenv').config();
 
 const watcher = chokidar.watch(
-    process.env.PATHS,
+    '../../../../_Downloaded Content/test',
     { persistent: true, awaitWriteFinish: true }
 );
 
@@ -13,6 +13,6 @@ watcher.on('add', function(path) {
     const extension = parse(path).ext
     const dir = dirname(path)
     const normalizedName = name.replaceAll('.', ' ').split('1080p')[0].split('WEBRip')[0].split('720p')[0].trim('')
-
+    
     rename(path, resolve(dir, normalizedName + extension), (err) => {})
 })
