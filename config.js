63ed964aa702dea5d9656ed5ee2492edfbb2403d
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-03 02:40:01 -0800

    created config file

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // RENAME CONFIG
+    dirsToRename: [
+        '../../../../_Downloaded Content/Movies',
+        '../../../../_Downloaded Content/Shows',
+        '../../../../_Downloaded Content/Subtitles'
+    ],
+    // SORT CONFIG
+    dirToSort: '../../../../_Downloaded Content/Movies',
+    sortByAge: true,
+    sortByName: false,
+    sortByLength: false,
+    reverse: true,
+}
