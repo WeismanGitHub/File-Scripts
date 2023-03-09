e7053a1bcb759dab161e9539b75df8ffa774d977
WeismanGitHub <102398620+WeismanGitHub@users.noreply.github.com>
2023-03-08 23:35:48 -0800

    added more config options for count-files.js

--- config.js
+++ config.js
@@ -9,12 +9,14 @@ module.exports = {
     moviesDirectory: '../../../../_Downloaded Content/Movies',
     subTitlesDirectory: '../../../../_Downloaded Content/Subtitles',
     words: ['fuck', 'shit', 'ass', 'crap', 'bitch', 'cunt'],
-    byReleaseYear: false, // Sorts by release date at the end of file name. EX: The Thing 1982
+    byReleaseYear: true, // Sorts by release date at the end of file name. EX: The Thing 1982
     byName: false,
     byLength: false,
     bySize: false,
     byFileAge: false,
-    byWords: true,
+    byWords: false,
     reverse: false,
     walkDirectories: false,
+    returnCount: true, // Return the count, or return the matches.
+    returnSortedByCount: true,
 }
